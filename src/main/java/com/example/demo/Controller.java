7836037a664636105d8d7d7c6bd70cab00ba59b0
Yash Karnik <yashkarnik2000@gmail.com>
2024-09-22 13:15:48 +0530

    changes

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,13 +3,17 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Annotation 
 @RestController
-// Main class
 public class Controller {
 
     @GetMapping
-    public String helloGFG() {
-        return "Hello GeeksForGeeks";
+    public String hello() {
+        return "Hello world";
+    }
+
+    @GetMapping("/env")
+    public String envvar() {
+        String value_name = System.getEnv("TEST_CONN_STR");
+        return value_name;
     }
 }
